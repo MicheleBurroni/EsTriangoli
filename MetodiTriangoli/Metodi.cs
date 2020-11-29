ad504c3670abdc94409468336f04820d127e1139
michiburro14@gmail.com <michiburro14@gmail.com>
2020-11-29 16:25:48 +0100

    Add project files.

---
+++ MetodiTriangoli/Metodi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LibreriaTriangoli
+{
+    public class MetodiTriangoli
+    {
+        public static double MisuraLato()
+        {
+            Console.WriteLine("Inserire lunghezza lato");
+            double l = Convert.ToDouble(Console.ReadLine());
+            return l;
+        }
+        public static string RiconosciTriangoli(double l1, double l2, double l3)
+        {
+            string risultato = "";
+            if (l1 > 0 && l2 > 0 && l3 > 0)
+            {
+                if (l1 == l2 && l1 == l3)
+                {
+                    risultato = "Triangolo Equilatero!";
+                }
+                else if (l1 != l2 && l2 != l3 && l3 != l1)
+                {
+                    risultato = "Triangolo Scaleno!";
+                }
+                else
+                {
+                    risultato = "Triangolo Isoscele!";
+                }
+            }
+            else
+                risultato = "IMPOSSIBILE!";
+            return risultato;
+        }
+        public static double Perimetro(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+    }
+}
+
---
+++ MetodiTriangoli/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LibreriaTriangoli;
+
+namespace TriangoliMetodi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double l1 = MetodiTriangoli.MisuraLato();
+            double l2 = MetodiTriangoli.MisuraLato();
+            double l3 = MetodiTriangoli.MisuraLato();
+            double perimetro = MetodiTriangoli.Perimetro(l1, l2, l3);
+            string risultato = MetodiTriangoli.RiconosciTriangoli(l1, l2, l3);
+            if (l1 < 0 || l2 < 0 || l3 < 0)
+                Console.WriteLine("IMPOSSIBILE! Dati non validi!");
+            else
+                Console.WriteLine($"{risultato},perimetro = {perimetro}");
+
+        }
+    }
+}
